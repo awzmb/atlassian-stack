076a3e1c3b0edc5fa1ac29ff7d3a3151935a3f65
awzmb <bundschuh.dennis@gmail.com>
2023-06-21 16:08:47 +0000

    feat(devbox): switched to alpine base image for devbox

--- atlassian-devbox/build-image.sh
+++ atlassian-devbox/build-image.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )"
 
-podman build -t lscr.io/linuxserver/code-server:latest ${DIR}
+podman build -t devbox:latest ${DIR}
